240e0b7e1d4ae10c913b1e11ef9584637a517e92
karun <karun@chingari.io>
2023-05-01 02:33:39 +0530

    class based api view

---
+++ product/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import ProductAPI
+
+urlpatterns = [
+    path('products',ProductAPI.as_view(), name='Products')
+    
+    
+]
+    # path('product',product, name='product'),
+    # path('products',products,name='product'),
+    # path('add-product',add_product,name='Add Product'),
+    # path('update-product/<id>',update_product,name='Update Product'),
+    # path('delete-product/<id>',delete_product,name='Delete Product'),
+    # path('get-book',get_books,name='Get Book'),
+
